99b64f99d83e803451f26236112d363cae55267d
yansalim <yan_salim@hotmail.com>
2023-10-03 10:07:25 -0300

    feat - insert data in table

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
 
 const form = document.getElementById('form-materia');   
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji Celebrando"'
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji Triste"'
+let linhas = '';
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -8,15 +11,20 @@ form.addEventListener('submit', function (e) {
     const inputNomeMateria = document.getElementById("nome-materia");
     const inputNotaMateria = document.getElementById("nota-materia");
 
+
     let linha = '<tr>'
     linha += `<td>${inputNomeMateria.value}</td>`;
     linha += `<td>${inputNotaMateria.value}</td>`;
-    linha += `<td>${inputNotaMateria.value >=7 ? 'Aprovado' : 'Reprovado'} </td>`;
+    linha += `<td>${inputNotaMateria.value >=7 ? imgAprovado : imgReprovado} </td>`;
     linha += `</tr>`;
 
-const corpoTabela = document.querySelector('tbody');
-corpoTabela.innerHTML = linha
+    linhas += linha;
 
-    alert(`Materia: ${inputNomeMateria.value} - Nota: ${inputNotaMateria.value}`);
+const corpoTabela = document.querySelector('tbody');
+corpoTabela.innerHTML = linhas
+
+inputNomeMateria.value = ''
+inputNotaMateria.value = ''
+  //  alert(`Materia: ${inputNomeMateria.value} - Nota: ${inputNotaMateria.value}`);
 
 });
