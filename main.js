798d6f12249d5f56ecab79eae8463c4733a7290e
yansalim <yan_salim@hotmail.com>
2023-10-31 11:28:04 -0300

    feat ( add ling, table and media)

--- main.js
+++ main.js
@@ -3,13 +3,24 @@
 const form = document.getElementById('form-materia');   
 const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji Celebrando"'
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji Triste"'
-let linhas = '';
+const materias [];
+const notas [];
+    let linhas = '';
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
+    adicionaLinha();
+    atualizaTabela();
+    atualizaMediaFinal();
+});
+
+function adicionaLinha(){
     const inputNomeMateria = document.getElementById("nome-materia");
     const inputNotaMateria = document.getElementById("nota-materia");
+
+    materias.push(inputNomeMateria.value);
+    notas.push(inputNotaMateria.value);
 
 
     let linha = '<tr>'
@@ -20,11 +31,21 @@ form.addEventListener('submit', function (e) {
 
     linhas += linha;
 
+
+
+inputNomeMateria.value = '';
+inputNotaMateria.value = '';
+
+
+}
+
+function atualizaTabela(){
+
 const corpoTabela = document.querySelector('tbody');
-corpoTabela.innerHTML = linhas
+corpoTabela.innerHTML = linhas;
+}
 
-inputNomeMateria.value = ''
-inputNotaMateria.value = ''
-  //  alert(`Materia: ${inputNomeMateria.value} - Nota: ${inputNotaMateria.value}`);
-
-});
+function atualizaMediaFinal(){
+    console.log (materias);
+    console.log (notas);
+}
